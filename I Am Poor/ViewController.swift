b17f8c17ff5bf2784517c4d206d99ec270116010
Matiny L <matinylouis@gmail.com>
2020-02-21 21:15:40 -0500

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Matiny on 2/21/20.
+//  Copyright © 2020 Matiny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
